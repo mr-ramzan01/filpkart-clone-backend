b45a22f0569e4af12c0233ec5d537fa8cf703dd5
mr-ramzan01 <ramzankhanmansoori0786@gmail.com>
2022-11-24 17:41:43 +0530

    products done

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -8,9 +8,7 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 const connection = async () => {
-    // await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.zopdele.mongodb.net/?retryWrites=true&w=majority`);
-    await mongoose.connect("mongodb+srv://check:check123@cluster0.kh22har.mongodb.net/?retryWrites=true&w=majority");
-    // await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.zopdele.mongodb.net/?retryWrites=true&w=majority`);
     console.log("DB connected");
 }
 
