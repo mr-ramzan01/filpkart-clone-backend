b272aa035b0a66b9d113af5d31a1b675799a7897
senCoder12 <mayukhsen85082@gmail.com>
2022-11-25 22:42:27 +0530

    order cart

--- Backend/Models/cartItem.model.js
+++ Backend/Models/cartItem.model.js
@@ -3,32 +3,97 @@ import mongoose from "mongoose";
 const cartItemSchema = new mongoose.Schema(
   {
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-    },
-    itemName: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
-    itemId: {
+    category_id: {
+      type: Number,
+      required: true
+    },
+    category_name: {
       type: String,
-      required: true,
+      required: true
     },
     image: {
       type: String,
+      required: true
     },
-    price: {
+    description: {
+      type: String,
+      required: true
+    },
+    stars: {
+      type: Number,
+      required: false
+    },
+    ratings: {
+      type: String,
+      required: false
+    },
+    reviews: {
+      type: String,
+      required: false
+    },
+    warranty: {
+      type: String,
+      required: false
+    },
+    new_price: {
+      type: Number,
+      required: true
+    },
+    old_price: {
+      type: Number,
+      required: true
+    },
+    discount: {
+      type: Number,
+      required: true
+    },
+    delivery_type: {
+      type: String,
+      required: true
+    },
+    offer: {
+      type: String,
+      required: false
+    },
+    offer2: {
+      type: String,
+      required: false
+    },
+    hidden_stars: {
+      type: Number,
+      required: true
+    },
+    item_id: {
       type: Number,
       required: true
     },
     quantity: {
-        type: Number,
-        default: 1,
+      type: Number,
+      required: false
+    },
+    size: {
+      type: String,
+      required: false
+    },
+    more_data: {
+      type: String,
+      required: false
+    },
+    color: {
+      type: String,
+      required: false
+    },
+    brand: {
+      type: String,
+      required: false
     }
   },
   {
-      versionKey: false,
-      timestamps: true
+    versionKey: false,
+    timestamps: true
   }
 );
 const cartItemModel = mongoose.model("cartItem", cartItemSchema);
